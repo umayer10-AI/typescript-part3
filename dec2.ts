5c519c26b65e70407e7b449ed6365f789da59176
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 18:20:17 +0600

    add

--- dec2.ts
+++ dec2.ts
@@ -1,6 +1,12 @@
-
+const up = (t: any, key: string, descriptor: PropertyDescriptor) => {
+    descriptor.value = function dsum(x: number ,y: number){
+        let b = x+y
+        return "the Output of "+x+ " and " +y+" is "+ b
+    }
+}
 
 class A {
+    @up
     sum(x: number ,y: number){
         return x+y
     }
