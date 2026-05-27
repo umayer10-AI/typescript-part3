54653a3a1c91f98761444dd11d158b83e1929428
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 15:31:37 +0600

    add

---
+++ app.ts
@@ -0,0 +1,16 @@
+type User = {
+    mobile: number,
+    id: number,
+    mark: number,
+    age: number,
+    semester: number,
+}
+
+let a: User = {
+    mobile: 9999,
+    id: 12,
+    mark: 90,
+    age: 21,
+    semester: 3,
+}
+console.log(a)
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    mobile: 9999,
+    id: 12,
+    mark: 90,
+    age: 21,
+    semester: 3,
+};
+console.log(a);
 //# sourceMappingURL=app.js.map
