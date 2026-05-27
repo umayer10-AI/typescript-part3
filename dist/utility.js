6cb52f18ca10f311cb2d0293f1fbd5aaa31effc1
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 15:46:15 +0600

    add

---
+++ dist/utility.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    name: "Umayer",
+    location: "Fatullah",
+    students: 200,
+};
+//# sourceMappingURL=utility.js.map
---
+++ utility.ts
@@ -0,0 +1,12 @@
+interface A {
+    name: string,
+    location: string,
+    students: number,
+    branch: number
+}
+
+let a: A = {
+    name: "Umayer",
+    location: "Fatullah",
+    students: 200,
+}
