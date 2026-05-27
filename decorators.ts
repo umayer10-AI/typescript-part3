98c99fad37080e9ef9c6cf090db6fad2e0e88111
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 17:22:59 +0600

    add

---
+++ decorators.ts
@@ -0,0 +1,8 @@
+class A {
+    constructor(public x: number, public y: number){
+
+    }
+}
+
+let a = new A(10,20)
+console.log(a)
--- dist/decorators.js
+++ dist/decorators.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class A {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+let a = new A(10, 20);
+console.log(a);
 //# sourceMappingURL=decorators.js.map
